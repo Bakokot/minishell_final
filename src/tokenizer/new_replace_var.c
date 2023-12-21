205322a2047e8d4c0e40c99ba9a810ba1b02eaa6
Thibault Barde-Cabucon <tbarde-c@student.42lyon.fr>
2023-12-21 10:52:01 +0100

    Revert "revert change on the $VAR management"
    This reverts commit e4e77a4cc2296acf7369821839c110925f61a315.

--- src/tokenizer/new_replace_var.c
+++ src/tokenizer/new_replace_var.c
@@ -6,7 +6,7 @@
 /*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:49:23 by tbarde-c          #+#    #+#             */
-/*   Updated: 2023/12/21 10:50:17 by thibault         ###   ########.fr       */
+/*   Updated: 2023/12/21 09:24:22 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ char *updated_token, t_env *env)
 	marker = *i;
 	while (token[*i] && token[*i] != '\"' && token[*i] != '\"')
 	{
-		if (token[*i] == '$')
+		if (token[*i] == '$' && lookup_values(token + 1, env) != NULL)
 		{
 			updated_token = update_token(token, i, marker, updated_token);
 			*i += 1;
