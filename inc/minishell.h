62d14db193e351ad5df75a42039d2dcafd148ec0
Thibault Barde-Cabucon <tbarde-c@student.42lyon.fr>
2023-12-27 00:31:59 +0100

    norminette .h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 13:31:31 by tbarde-c          #+#    #+#             */
-/*   Updated: 2023/12/26 19:55:41 by thibault         ###   ########.fr       */
+/*   Updated: 2023/12/27 00:31:36 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,6 @@
 # include "../ft_printf/ft_printf.h"
 # include "../libft/inc/libft.h"
 
-# define SUCCESS 0
-# define ERROR 404
-
 /**
  * ERROR MESSAGES
 */
@@ -44,7 +41,7 @@
 # define IN_SHELL 1
 # define IN_PROGRAM 2
 
-extern int g_exit_status;
+extern int	g_exit_status;
 
 /*
 		LITTERAL	--> (0)		An argument
@@ -88,170 +85,135 @@ typedef struct s_heredoc
 
 typedef struct s_exec
 {
-	char	**args;
-	char	**envp;
-	char	*path;
-	int		fd_heredoc;
-	int		sstdin;
-	int		sstdout;
-	int		in;
-	int		out;
+	char		**args;
+	char		**envp;
+	char		*path;
+	int			fd_heredoc;
+	int			sstdin;
+	int			sstdout;
+	int			in;
+	int			out;
 	t_heredoc	*hd;
 }	t_exec;
 
+/**
+ * Manage new line
+*/
+char		*read_new_line(char *line);
+char		*read_new_line_pipe(char *line);
 
 /**
  * Tokenizer
 */
-t_token	**tokenize(char *line, t_env *env);
-void	replace_vars(t_token **token_lst, t_env *env);
-void	skip_single_quotes(int *i, char *str);
-bool	single_dollar(t_token *token);
-char	*update_dollarless_token(t_token *token, int i, \
-int marker, char *new_token);
-
-/**
- * New Tokenizer fubction (need to delete some previous functions)
-*/
-void	replace_varsn(t_token **token_lst, t_env *env);
-char	*update_token(char *token, int *i, int marker, char *updated_token);
-char	*change_var(char *token, int *i, char *updated_token, t_env *env);
-char	*single_dollarn(char *updated_token);
-char	*double_dollar(char *updated_token);
-char	*exit_status_var(char *updated_token);
-bool	special_cases(char *token, char **updated_token, int *i);
-void	remove_token_quotes(t_token **token_lst);
+t_token		**tokenize(char *line, t_env *env);
+void		replace_varsn(t_token **token_lst, t_env *env);
+char		*update_token(char *token, int *i, int marker, char *updated_token);
+char		*change_var(char *token, int *i, char *updated_token, t_env *env);
+char		*single_dollarn(char *updated_token);
+char		*double_dollar(char *updated_token);
+char		*exit_status_var(char *updated_token);
+bool		special_cases(char *token, char **updated_token, int *i);
+void		remove_token_quotes(t_token **token_lst);
 
 /**
  * t_token struct utils
 */
-t_token	*create_new_token(char *str, int type);
-void	add_back_token(t_token **token_lst, t_token *token);
-t_token	*get_last_token(t_token *lst);
-void	delete_first_token(t_token **token_lst);
-char	*add_litteral_dollar(char *dollarless_token, int *i, char *tkn);
-void	set_new_token(t_token *token, char *dollarless);
-size_t	token_size(t_token *token_lst);
-char	**token_lst_into_char(t_token *token_lst);
-void	delete_token(t_token **token_lst, t_token *token);
+t_token		*create_new_token(char *str, int type);
+void		add_back_token(t_token **token_lst, t_token *token);
+t_token		*get_last_token(t_token *lst);
+void		delete_first_token(t_token **token_lst);
+size_t		token_size(t_token *token_lst);
+char		**token_lst_into_char(t_token *token_lst);
+void		delete_token(t_token **token_lst, t_token *token);
 
 /**
  * Environment
 */
-t_env	*init_env(char **envp);
+t_env		*init_env(char **envp);
 
 /**
  * t_env struct utils
 */
-t_env	*get_last_env(t_env *lst);
-t_env	*create_new_env(char *key, char *values);
-char	*get_env_key(char *envp);
-char	*get_env_values(char *envp);
-char	**env_lst_into_char(t_env *env);
-size_t	env_size(t_env *env);
-char	*lookup_values(char *key, t_env *env);
-void	add_back_env(t_env **env_lst, t_env *env);
+t_env		*get_last_env(t_env *lst);
+t_env		*create_new_env(char *key, char *values);
+char		*get_env_key(char *envp);
+char		*get_env_values(char *envp);
+char		**env_lst_into_char(t_env *env);
+size_t		env_size(t_env *env);
+char		*lookup_values(char *key, t_env *env);
+void		add_back_env(t_env **env_lst, t_env *env);
 
 /**
  * Utils
 */
-int		ft_isspace(char c);
-int		is_quote(char c);
-int		is_metachar(char c);
-int		is_double_redirection(char *str);
-char	*ft_strndup(char *s1, size_t size);
-int		ft_strcmp(char *s1, char *s2);
-void	free_array(char **arr);
-bool	closed_quotes(char *str);
-bool 	unclosed_quotes(char *line);
-bool	unclosed_pipe(char *line);
-char	strchr_quote(char *line);
+int			ft_isspace(char c);
+int			is_quote(char c);
+int			is_metachar(char c);
+int			is_double_redirection(char *str);
+char		*ft_strndup(char *s1, size_t size);
+int			ft_strcmp(char *s1, char *s2);
+void		free_array(char **arr);
+bool		closed_quotes(char *str);
+bool		unclosed_quotes(char *line);
+bool		unclosed_pipe(char *line);
+char		strchr_quote(char *line);
 
 //Builtins
-int		pwd(void);
-int		env_builtin(t_env *env);
-int		echo(t_token *token_lst);
-void	print_export(t_env *env);
-int		cd(t_token	*token_lst, t_env *env);
-int		unset(t_token *token_lst, t_env **env);
-int		export(t_token *token_lst, t_env **env);
-int	ft_exit(t_env *env, t_token **token, t_exec *exec);
+int			pwd(void);
+int			env_builtin(t_env *env);
+int			echo(t_token *token_lst);
+void		print_export(t_env *env);
+int			cd(t_token	*token_lst, t_env *env);
+int			unset(t_token *token_lst, t_env **env);
+int			export(t_token *token_lst, t_env **env);
+int			ft_exit(t_env *env, t_token **token, t_exec *exec);
 
 //Exec
-void	init_exec(t_exec *exec, t_heredoc *hd);
-void	free_exec(t_exec *exec, t_token *token);
-void	handle_command(t_token **token_lst, t_env *env);
-int		check_args(t_token *token);
-void	exec_command(t_exec	*exec);
-int		is_bultin(char *cmd);
-int		exec_bultin(t_token *token_lst, t_env *env);
-int		exec_exit(t_token **token, t_env *env, t_exec *exec);
+void		init_exec(t_exec *exec, t_heredoc *hd);
+void		free_exec(t_exec *exec, t_token *token);
+void		handle_command(t_token **token_lst, t_env *env);
+int			check_args(t_token *token);
+void		exec_command(t_exec	*exec);
+int			is_bultin(char *cmd);
+int			exec_bultin(t_token *token_lst, t_env *env);
+int			exec_exit(t_token **token, t_env *env, t_exec *exec);
+char		*check_directory(char *path);
+void		free_all_child(t_token **token, t_env *env, int **pipes, int *pid);
 
 //Redirections
-t_exec	*handle_redirection(t_token **token_lst, t_env *env, t_heredoc *hd, int i);
+t_exec		*handle_redirection(t_token **token_lst, t_env *env, \
+t_heredoc *hd, int i);
 t_heredoc	*check_heredocs(t_token	*token, t_env *env);
-int		end_heredoc(t_token *token, char *line);
-int		change_standard_fd(t_token *token_lst, t_exec *exec);
-void	remove_redirection(t_token **head);
-void	restore_fd(t_exec *exec, t_token *token);
+int			end_heredoc(t_token *token, char *line);
+int			change_standard_fd(t_token *token_lst, t_exec *exec);
+void		remove_redirection(t_token **head);
+void		restore_fd(t_exec *exec, t_token *token);
 
 //Pipex + Utils
-int		pipex(t_token **token, t_env *env, int count, t_heredoc *hd);
-size_t	count_pipes(t_token *token);
-int		**setup_pipes(t_token *token);
-void	free_pipes(int **pipes);
-void	close_pipes(int **pipes, int limiter);
-t_token	*get_command(t_token *token, int index);
-int		translate_error(int	nbr);
+int			pipex(t_token **token, t_env *env, int count, t_heredoc *hd);
+size_t		count_pipes(t_token *token);
+int			**setup_pipes(t_token *token);
+void		free_pipes(int **pipes);
+void		close_pipes(int **pipes, int limiter);
+t_token		*get_command(t_token *token, int index);
+int			translate_error(int nbr);
 
 /**
  * Freeing
 */
-void	free_all(t_token **token_lst, t_env *env);
-void	free_all_env(t_env *env_lst);
-void	free_all_token(t_token **token_lst);
+void		free_all(t_token **token_lst, t_env *env);
+void		free_all_env(t_env *env_lst);
+void		free_all_token(t_token **token_lst);
 
 /**
  * Error management
 */
-void	log_error(char *str);
+void		log_error(char *str);
 
 /**
  * Signals management
 */
-void	signal_handling(int status);
-void	ctrl_d_handler(char *line, t_token **token_lst, t_env *env);
-
-/**
- * Garbage collector  -- TO ERASE
-*/
-typedef struct s_gc_node
-{
-	struct s_gc_node	*prev;
-	struct s_gc_node	*next;
-	size_t				size;
-}	t_gc_node;
-
-/**
- * We keep the adress of the first and last node
- * to "travel" faster in the structure
-*/
-typedef struct s_gc_glob
-{
-	t_gc_node	*first_node;
-	t_gc_node	*last_node;
-	size_t		total_size;
-	size_t		nb_allocs;
-}	t_gc_glob;
-
-void	gc_init(t_gc_glob *gc);
-void	*gc_malloc(t_gc_glob *gc, size_t size);
-void	gc_free(t_gc_glob *gc, void **ptr);
-void	gc_free_all(t_gc_glob *gc);
-
-/**
- * TO ERASE IN THE END
-*/
-void	print_env(t_env *env);
+void		signal_handling(int status);
+void		ctrl_d_handler(char *line, t_token **token_lst, t_env *env);
 
 #endif
